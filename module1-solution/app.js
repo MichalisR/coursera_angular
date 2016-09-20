e8bc57ec171b3d300879451d81736883d97e949d
Michalis Rodios <mro@agilis-sa.com>
2016-09-20 13:14:09 +0300

    Fixed minification problem

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,7 +1,7 @@
-(function () {
-'use strict';
-
-angular.module('LunchCheck', []);
+(function(){
+	'use-strict'
+	angular
+	.module("LunchCheck", [])
 
 .controller("LunchCheckController", LunchCheckFunction);
 
